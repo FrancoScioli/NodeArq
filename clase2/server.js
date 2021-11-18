6e5d305e3b80f95641181b4c275655f2acd3c295
FrancoScioli <franco.scioli.13@gmail.com>
2021-11-18 15:21:06 -0300

    conexion db

--- clase2/server.js
+++ clase2/server.js
@@ -4,13 +4,22 @@ let {EOL} = require('os');
 let _ = require('underscore')
 let nodemon = require('nodemon')
 
+const mysql = require('mysql')
+const myConnection  = require('express-myconnection')
 
 let app = express()
 app.use(express.json())
 
 app.use(express.static('public'))
 app.use('/static', express.static(__dirname + '/public'))
+app.use(myConnection(mysql,{
+    host:'localhost',
+    user:'root',
+    password:'contraseña',
+    port:'3306',
+    database:'lossopranos'
 
+},'single'))
 
 app.get('/procesar', (req,res)=>{
     res.setHeader('Content-Type','application/json')
